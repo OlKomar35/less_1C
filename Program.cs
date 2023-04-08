e9f01535445812f80ba84025b8a890d0e20c5483
Olga Komar <komar_ol@mail.ru>
2023-04-08 18:32:13 +0300

    added added solution to task 1

--- Program.cs
+++ Program.cs
@@ -5,3 +5,30 @@
 //a = 2 b = 10 -> max = 10
 //a = -9 b = -3 -> max = -3
 
+int a1 = 5;
+int b1 = 7;
+int max = a1;
+if (max < b1) { max = b1; }
+Console.WriteLine($"max= {max}");
+
+int a2 = 2, b2 = 10;
+if (a2 < b2)
+{
+    Console.WriteLine($"max= {b2}");
+}
+else
+{
+    Console.WriteLine($"max= {a2}");
+}
+
+
+Console.Write("Введите число a= ");
+int a3 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число b= ");
+int b3 = Convert.ToInt32(Console.ReadLine());
+if (a3 > b3){
+    Console.WriteLine($"большее число а= {a3}");
+}else {
+  Console.WriteLine($"большее число b= {b3}");  
+}
+
